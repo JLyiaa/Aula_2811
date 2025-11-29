82ec3c21c58b4647fad34ae4a42cd97ae4d2fed9
JLyiaa <jullya.g.honorio@gmail.com>
2025-11-28 21:30:24 -0300

    minha primeira vez no git hub

---
+++ calculadora.py
@@ -0,0 +1,6 @@
+a = float(input("entre com o primeiro numero: "))
+b = float(input("entre com o segundo numero: "))
+soma = a + b
+subtracao = a - b
+print(f"a soma é {soma}.")
+print(f"a subtração é {subtracao}.")
