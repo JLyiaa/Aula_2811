102dbe46292b4a21eaf4cdc14caee57f43c3d18e
JLyiaa <jullya.g.honorio@gmail.com>
2025-11-28 22:11:31 -0300

    minha primeira vez no git hub

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,6 @@
 a = float(input("entre com o primeiro numero: "))
 b = float(input("entre com o segundo numero: "))
 soma = a + b
-subtracao = a - b
+divizao = a / b
 print(f"a soma é {soma}.")
-print(f"a subtração é {subtracao}.")
+print(f"a divizão é {divizao}.")
